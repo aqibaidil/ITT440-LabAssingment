32f59c4d51f2155886b4388d56bc5d73248155b4
aqib <aqib@aidil.com>
2022-05-17 08:04:54 +0000

    Lab 2 done

---
+++ 3.1.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <errno.h>
+#include <signal.h>
+
+int main (void)
+{
+	void 
--- userinput.c
+++ userinput.c
@@ -4,6 +4,7 @@ int main(void) {
 int age;
 printf("Hi, how old are you? > ");
 scanf("%d", &age);
-printf("You are %d years old", age);
+printf("You are %d years old\n", age);
 return (0);
+
 }
